59af3bcfd9804c3ceacac26154bc574e48807c2b
Justo Vargas <justo.vargas@wnco.com>
2014-02-21 13:26:52 -0600

    Justo Vargas - Added minimal funtionality

---
+++ dptos/src/main/java/com/dptosweb/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.dptosweb.service.impl;
+
+import com.dptosweb.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
